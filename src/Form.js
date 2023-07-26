07245c5c09777a34fcb7d19b27680dd84f91cebe
Zeeshan <zeeshik62.zk@gmail.com>
2023-07-26 12:51:53 +0500

    Price Section of report is done

--- src/Form.js
+++ src/Form.js
@@ -4,15 +4,22 @@ import './App.css';
 
 Modal.setAppElement('#root');
 
-const reportOptions = [
+
+
+// const reportOptions = [
   
-  { value: 'blood_test', label: 'Blood Test', price: 50 },
-  { value: 'blood_test', label: 'Blood Test', price: 50 }
+//   { value: 'blood_test', label: 'Blood Test', price: 50 },
+//   { value: 'blood_test', label: 'Blood Test', price: 50 }
   
 
-];
+// ];
 
-
+const priceData = {
+  'Blood Glucose Test': 30,
+  'Blood Gases Test': 50,
+  'Coagulation Test': 80,
+  // Add more options and prices as needed
+};
 
 
 const Form = ({ isOpen, onClose }) => {
@@ -26,12 +33,32 @@ const Form = ({ isOpen, onClose }) => {
     reportType: '',
   });
 
-  const [totalPrice, setTotalPrice] = useState(0);
-  const [dis,setDis]=useState(0);
+  const [selectedValue1, setSelectedValue1] = useState(null);
+  const [field1Options, setField1Options] = useState(['Blood', 'Heart', 'Kidney']);
+const [field2Options, setField2Options] = useState(['']);
+const handleField1Change = (event) => {
+  const selectedValue = event.target.value;
+
+  // Example logic: When selecting 'Option 1', update the options of the second field accordingly
+  if (selectedValue === 'Blood') {
+    setField2Options(['Blood Glucose Test', 'Blood Gases Test', 'Coagulation Test']);
+  } else if (selectedValue === 'Heart') {
+    setField2Options(['Electrocardiogram (ECG)', 'Magnetic resonance imaging (MRI)', 'Echocardiogram (ultrasound)']);
+  } else if (selectedValue === 'Kidney') {
+    setField2Options(['Kidney Test']);
+  }
+};
+  // const [totalPrice, setTotalPrice] = useState(0);
+  // const [dis,setDis]=useState(0);
   const [selectedOption, setSelectedOption] = useState('');
 
   function handleOptionChange(event) {
     setSelectedOption(event.target.value);}
+
+    function handleSelectChange(event){
+      setSelectedValue1(event.target.value)
+      console.log(selectedValue1)
+    }
 
 
 
@@ -44,10 +71,36 @@ const Form = ({ isOpen, onClose }) => {
   };
 
 
+
   const handleSubmit = (e) => {
     e.preventDefault();
     onClose();
   };
+ 
+  // const [price, setPrice] = useState(0);
+  // const selectedPrice = priceData[setSelectedValue1] || 0;
+  //   setPrice(selectedPrice);
+
+  let price = 0;
+
+  // Update the price based on the selected option
+  if (selectedValue1 === 'Blood Glucose Test') {
+    price = 10;
+  } else if (selectedValue1 === 'Blood Gases Test') {
+    price = 20;
+  } else if (selectedValue1 === 'Coagulation Test') {
+    price = 30;
+  }
+  else if (selectedValue1 === 'Electrocardiogram (ECG)') {
+    price = 40;
+  } else if (selectedValue1 === 'Magnetic resonance imaging (MRI)') {
+    price = 50;
+  }
+  else if (selectedValue1 === 'Echocardiogram (ultrasound)') {
+    price = 60;
+  } else if (selectedValue1 === 'Kidney Test') {
+    price = 70;
+  }
 
   return (
     <Modal
@@ -84,7 +137,7 @@ const Form = ({ isOpen, onClose }) => {
         </div>
         <div className="form-group">
           <label>Report:</label>
-          <select name="report" value={formData.report} >
+          {/* <select name="report" value={formData.report} >
           <option value="" disabled hidden>
           -- Select Report --
         </option>
@@ -94,9 +147,32 @@ const Form = ({ isOpen, onClose }) => {
                 {report.label}
               </option>
             ))}
-          </select>
+          </select> */}
+          <select onChange={handleField1Change}>
+          <option value="" >
+          -- Select Report --
+        </option>
+        {field1Options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+          
+        ))}
+        
+      </select>
+      
+
+          <label>Report Type:</label>
+      <select value={selectedValue1} onChange={handleSelectChange}>
+      
+        {field2Options.map((option, index) => (
+          <option key={index} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
         </div>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label>Report Type:</label>
           <select name="reportType" value={selectedOption} onChange={handleOptionChange}>
           <option value="" disabled hidden>
@@ -107,19 +183,17 @@ const Form = ({ isOpen, onClose }) => {
         <option value="option3">Blood gases test.</option>
            
           </select>
-        </div>
+        </div> */}
         <div className="total-price">
-         <p>Price: {selectedOption === 'option1' && <div>10,000 Rs</div>}
-         {selectedOption === 'option2' && <div>3000</div>}
-         {selectedOption === 'option3' && <div>2000</div>}
-         </p>
-         <p>Discount: {selectedOption === 'option1' && <div>500 Rs</div>}
-         {selectedOption === 'option2' && <div>300</div>}
-         {selectedOption === 'option3' && <div>200</div>}</p>
-         <p>Total:{selectedOption === 'option1' && <div>9500 Rs</div>}
-         {selectedOption === 'option2' && <div>2700</div>}
-         {selectedOption === 'option3' && <div>1800</div>}</p>
+          <u><p><b>Report Type Selected:</b> {selectedValue1}</p></u>
+         <p>Price: {price} Rs</p>
+         
+         
+         <p>Discount:</p>
+         <p>Total:</p>
+         
         </div>
+        
         <button type="submit">Submit</button>
         <button type="submit">Cancel</button>
       </form>
